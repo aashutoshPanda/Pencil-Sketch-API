9d51f6cef5a9fe859e24de3d43590ea4ed089231
aashutoshPanda <iamashutoshpanda@gmail.com>
2020-10-17 10:24:27 +0530

    first commit with heroku setup

---
+++ build/precache-manifest.bc0fb4ba1a07033423a2a38845071fad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96cf55f7630b5a1588fd75cb8f0fe648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e604a386ee7e9adcd92",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8461bcd469f56fce672",
+    "url": "/static/js/2.70742dab.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.70742dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e604a386ee7e9adcd92",
+    "url": "/static/js/main.5d419fe9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
